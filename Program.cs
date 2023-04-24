35e16cf44ac5a7e99e34f9d2d0e99fc76f35f35a
NgTrNamKhanh <109201894+NgTrNamKhanh@users.noreply.github.com>
2023-04-24 19:59:05 +0700

    add google auth

--- Program.cs
+++ Program.cs
@@ -4,13 +4,21 @@ using ASM.Data;
 using Microsoft.AspNetCore.Identity;
 using ASM.Repository;
 using System.Security.Policy;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 builder.Services.AddDbContext<ASMContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ASMContext") ?? throw new InvalidOperationException("Connection string 'ASMContext' not found.")));
-
+//Forwarded Headers Middleware middleware 
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 builder.Services
-    
 	.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
 	.AddEntityFrameworkStores<ASMContext>()
 	.AddDefaultUI()
@@ -20,7 +28,15 @@ builder.Services
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<ICartRepository, CartRepository>();
 builder.Services.AddTransient<IUserOrderRepository, UserOrderRepository>();
-
+//Add Google
+builder.Services.AddAuthentication()
+   .AddGoogle(options =>
+   {
+	   IConfigurationSection googleAuthNSection =
+	   config.GetSection("Authentication:Google");
+	   options.ClientId = googleAuthNSection["ClientId"];
+	   options.ClientSecret = googleAuthNSection["ClientSecret"];
+   });
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
@@ -28,13 +44,13 @@ using (var scope = app.Services.CreateScope())
 	await DbSeederRole.SeedDefaultData(scope.ServiceProvider);
 }
 
-
+app.UseForwardedHeaders();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 	{
 		app.UseExceptionHandler("/Home/Error");
-		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-		app.UseHsts();
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
 	}
 
 app.UseHttpsRedirection();
