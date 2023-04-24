d6a1fb583f2e42ff650a151dcfc3becbae7513bd
RileyReidx3 <59513246+RileyReidx3@users.noreply.github.com>
2023-04-24 20:35:31 +0700

    staff this cock

--- Controllers/StaffsController.cs
+++ Controllers/StaffsController.cs
@@ -55,6 +55,127 @@ namespace ASM.Controllers
             await _dbContext.SaveChangesAsync();
             return RedirectToAction("ViewOrders");
 		}
+
+        [HttpPost]
+        [ActionName("ShippedOrder")]
+        public async Task<IActionResult> ShippedOrder(int? id)
+        {
+            var staffId = GetUserId();
+            if (string.IsNullOrEmpty(staffId))
+            {
+                throw new Exception("User not logged in");
+            }
+            if (id == null || _dbContext.Order == null)
+            {
+                return NotFound();
+            }
+            var order = await _dbContext.Order
+            .Include(p => p.OrderStatus)
+            .FirstOrDefaultAsync(m => m.OrderId == id);
+            // Update the order status
+            order.OrderStatusID = 2;
+            order.StaffId = staffId;
+
+            _dbContext.Update(order);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("ViewOrders");
+        }
+        [HttpPost]
+        [ActionName("PendingOrder")]
+        public async Task<IActionResult> PendingOrder(int? id)
+        {
+            var staffId = GetUserId();
+            if (string.IsNullOrEmpty(staffId))
+            {
+                throw new Exception("User not logged in");
+            }
+            if (id == null || _dbContext.Order == null)
+            {
+                return NotFound();
+            }
+            var order = await _dbContext.Order
+            .Include(p => p.OrderStatus)
+            .FirstOrDefaultAsync(m => m.OrderId == id);
+            // Update the order status
+            order.OrderStatusID = 1;
+            order.StaffId = staffId;
+
+            _dbContext.Update(order);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("ViewOrders");
+        }
+        [HttpPost]
+        [ActionName("CancelledOrder")]
+        public async Task<IActionResult> CancelledOrder(int? id)
+        {
+            var staffId = GetUserId();
+            if (string.IsNullOrEmpty(staffId))
+            {
+                throw new Exception("User not logged in");
+            }
+            if (id == null || _dbContext.Order == null)
+            {
+                return NotFound();
+            }
+            var order = await _dbContext.Order
+            .Include(p => p.OrderStatus)
+            .FirstOrDefaultAsync(m => m.OrderId == id);
+            // Update the order status
+            order.OrderStatusID = 4;
+            order.StaffId = staffId;
+
+            _dbContext.Update(order);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("ViewOrders");
+        }
+        [HttpPost]
+        [ActionName("ReturnedOrder")]
+        public async Task<IActionResult> ReturnedOrder(int? id)
+        {
+            var staffId = GetUserId();
+            if (string.IsNullOrEmpty(staffId))
+            {
+                throw new Exception("User not logged in");
+            }
+            if (id == null || _dbContext.Order == null)
+            {
+                return NotFound();
+            }
+            var order = await _dbContext.Order
+            .Include(p => p.OrderStatus)
+            .FirstOrDefaultAsync(m => m.OrderId == id);
+            // Update the order status
+            order.OrderStatusID = 5;
+            order.StaffId = staffId;
+
+            _dbContext.Update(order);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("ViewOrders");
+        }
+        [HttpPost]
+        [ActionName("RefundOrder")]
+        public async Task<IActionResult> RefundOrder(int? id)
+        {
+            var staffId = GetUserId();
+            if (string.IsNullOrEmpty(staffId))
+            {
+                throw new Exception("User not logged in");
+            }
+            if (id == null || _dbContext.Order == null)
+            {
+                return NotFound();
+            }
+            var order = await _dbContext.Order
+            .Include(p => p.OrderStatus)
+            .FirstOrDefaultAsync(m => m.OrderId == id);
+            // Update the order status
+            order.OrderStatusID = 6;
+            order.StaffId = staffId;
+
+            _dbContext.Update(order);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToAction("ViewOrders");
+        }
         private string GetUserId()
         {
             var principal = _httpContextAccessor.HttpContext.User;
